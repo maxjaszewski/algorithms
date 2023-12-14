849dd1b7f4fff28f93a7b0aae97232f3dc9b416b
mjas0006 <mjas0006@student.monash.edu>
2023-12-15 10:36:18 +1100

    Fix walking down suffix tree

--- ukkonens/ukkonens.py
+++ ukkonens/ukkonens.py
@@ -57,6 +57,21 @@ class SuffixTree:
         node.end = end
         return node
 
+    def walk(self, i):
+        if (self.active_length == 0):
+            self.active_edge = i
+        elif self.active_node.children[self.string[self.active_edge]].start + self.active_length - 1 >= \
+                    self.active_node.children[self.string[self.active_edge]].end and not \
+                    self.active_node.children[self.string[self.active_edge]].is_leaf:
+                start = self.active_node.children[self.string[self.active_edge]].start
+                end = self.active_node.children[self.string[self.active_edge]].end
+                diff = end-start+1
+                self.active_node = self.active_node.children[self.string[self.active_edge]]
+                self.active_length -= diff
+                self.active_edge = self.active_edge + diff
+                self.walk(i)
+
+
     def extend(self, i: int):
         # update leaf end global with extension
         Node.leaf_end = i
@@ -66,59 +81,45 @@ class SuffixTree:
         # loop continues while inserting remaining suffixes
         # continue or break if match found and going to next char
         while self.remainder > 0:
-
             # active edge to current i if length 0
+            self.walk(i)
 
-            if self.active_length == 0:
-                self.active_edge = i
-            else:
-                # walk down
-                while self.active_node.children.get(self.string[self.active_edge]) and \
-                        self.active_node.children[self.string[self.active_edge]].start + self.active_length - 1 >= \
-                        self.active_node.children[self.string[self.active_edge]].end and not \
-                        self.active_node.children[self.string[self.active_edge]].is_leaf:
-                    start = self.active_node.children[self.string[self.active_edge]].start
-                    end = self.active_node.children[self.string[self.active_edge]].end
-                    diff = start-end+1
-                    self.active_node = self.active_node.children[self.string[self.active_edge]]
-                    self.active_length -= diff
-                    self.active_edge = self.active_edge + diff
-
-                    # no matching edge, create new leaf
+            # no matching edge, create new leaf
             if self.active_node.children.get(self.string[self.active_edge]) is None:
                 self.active_node.children[self.string[self.active_edge]] = self.create_node(start=i, end=None,
                                                                                             is_leaf=True)
-
             # matching edge
             else:
-                # next character from current position is matching
-                # increment active_length
+                # next character is matching break to next i
                 if self.string[self.active_node.children[self.string[self.active_edge]].start + self.active_length] == \
                         self.string[i]:
                     self.active_length += 1
                     break
-                    # check if walking to next node
 
-                # splitting from root node
+                # next character mismatch, must split
                 curr_pos = self.active_node.children[self.string[self.active_edge]].start + self.active_length
+                # new intermediate node
                 split_node = self.create_node(
                     start=self.active_node.children[self.string[self.active_edge]].start,
                     end=curr_pos - 1,
                     is_leaf=False)
+                # organise children of node
                 split_node.children[self.string[curr_pos]] = self.active_node.children[self.string[self.active_edge]]
                 split_node.children[self.string[i]] = self.create_node(
                     start=i,
                     end=None,
                     is_leaf=True)
                 self.active_node.children[self.string[self.active_edge]].start = curr_pos
+                # repoint to split node
                 self.active_node.children[self.string[self.active_edge]] = split_node
 
                 # update suffix link if new node has already been inserted
                 if self.last_new_node:
                     self.last_new_node.suffix_link = split_node
-
+                # update last new node for next suffix link
                 self.last_new_node = split_node
 
+                # directing where to next
                 if self.active_node == self.root:
                     self.active_edge = i - self.remainder + 2
                     self.active_length -= 1
@@ -126,10 +127,11 @@ class SuffixTree:
                     self.active_node = self.active_node.suffix_link
                 else:
                     self.active_node = self.root
+
             # at this point have inserted split/leaf
             self.remainder -= 1
 
 
 if __name__ == "__main__":
     suffix_tree = SuffixTree()
-    suffix_tree.build("pallpall")
+    suffix_tree.build("abcabxabcd")
