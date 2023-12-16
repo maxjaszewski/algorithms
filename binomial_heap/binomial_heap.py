398175943156b120fe28fd09cc72c19d06b75008
mjas0006 <mjas0006@student.monash.edu>
2023-12-17 10:46:47 +1100

    binomial heap: checkpoint

--- binomial_heap/binomial_heap.py
+++ binomial_heap/binomial_heap.py
@@ -14,22 +14,45 @@ class BinomialHeap:
         Create new node with key and insert
         :param key: value of key
         """
-        heap = BinomialHeap(key)
-        self.merge(heap)
+        heap = BinomialHeap()
+        heap.head = Node(key)
+        self.head = merge(self, heap)
 
     def min(self):
         """
         Get minimum key value
         :return: Node object
         """
-        pass  # todo
+        # Track the parent as well so min_node can be removed when extracting minimum value
+        minimum, min_node, min_node_left = float('-inf'), None, None
+        prev, curr = None, self.head
+        while curr is not None:
+            if curr.key < minimum:
+                minimum, min_node, min_node_left = curr.key, curr, prev
+            prev, curr = curr, curr.sibling
+        return min_node, min_node_left
 
     def extract_min(self):
         """
         Get and remove minimum value
-        :return: Minimum integer
+        :return: Node object
         """
-        pass  # todo
+        min_node, min_node_left = self.min()
+        if min_node_left is None:
+            self.head = min_node.sibling
+        else:
+            min_node_left.sibling = min_node.sibling
+
+        children_heap = BinomialHeap()
+        children_heap.head = min_node.child
+        children_heap.reverse()
+        self.head = union(self, children_heap)
+
+        min_node.child = None
+        min_node.degree = None
+        min_node.sibling = None
+        return min_node
+
 
     def decrease_key(self, node, key):
         """
@@ -49,34 +72,22 @@ class BinomialHeap:
         """
         pass  # todo
 
+    def reverse(self):
+        start = self.head
+        while start is not None:
+            
+            start = start.sibling
+        self.head = start
 
 """
                                             -- FUNCTIONS --
 """
-
-
-def union(h1, h2):
-    """
-    Join two Binomial Heaps
-    :param h1: First
-    :param h2: Second
-    :return: Union heap
-    """
-    res_heap = BinomialHeap()
-    res_heap.head = merge(h1, h2)
-    prev, curr, next = None, res_heap.head, res_heap.head.sibling
-
-
-
-
-
-
 def merge(h1, h2):
     """
     Merge two binomial heaps into one linked list with roots in monotonically increasing order
     :param h1: Head of first binomial heap
     :param h2: Head of second binomial heap
-    :return:
+    :return: Merged heap
     """
     p1, p2 = h1.head, h2.head
     curr, res = None, None
@@ -119,3 +130,36 @@ def link(x, y):
     y.degree += 1
     x.parent = y
     y.child = x
+
+def union(h1, h2):
+    """
+    Join two Binomial Heaps
+    :param h1: First
+    :param h2: Second
+    :return: Union heap
+    """
+    res_heap = BinomialHeap()
+    res_heap.head = merge(h1, h2)
+    if res_heap.head is None:
+        return res_heap
+    prev, curr, next = None, res_heap.head, res_heap.head.sibling
+    while next is not None:
+        if (curr.degree != next.degree) or (next.sibling and next.degree == next.sibling.degree):
+            prev, curr, next = curr, next, next.sibling
+        else:
+            if curr.key <= next.key:
+                curr.sibling = next.sibling
+                link(next, curr)
+            else:
+                if prev is not None:
+                    prev.sibling = next
+                else:
+                    res_heap.head = next
+                link(curr, next)
+
+    return res_heap
+
+
+
+
+
